5e38c1e8a6e2b319290ea79c5f7f74f0e4c38d9d
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-10-15 02:17:59 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,13 +3,10 @@ def my_select(collection)
  counter = 0
  collected = []
  while counter != collection.length
-
   if yield(collection[counter]) == true
     collected.push(collection[counter])
   end
-
   counter += 1
  end
-
  return collected
 end
