b2b909ffe90978027be297127c705b73301cce29
Katerina  Papacostas <katpapacostas@gmail.com>
2017-10-23 02:25:25 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
 def my_select(collection)
- # your code here!
+  return "This block should not run!" if collection.length == 0
+
+  if block_given?
+    i = 0
+    times = collection.length - 1
+    new_array = []
+
+    while i <= times
+      new_elem = (yield collection[i])
+      if new_elem == true
+        new_array.push(collection[i])
+      end
+      i += 1
+    end
+
+    new_array
+  else
+    "No block was given!"
+  end
 end
